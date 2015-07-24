6f5afedfeab08bdd815bf45b1e87fa2cf4b1274e
saan1984 <sandeep_giet@yahoo.com>
2015-07-25 00:42:37 +0530

    changes

--- main.js
+++ main.js
@@ -1,15 +1,13 @@
 var application = require('app'),
-    BrowserWindow = require('browser-window');
-var TrayMenu = require('./js/tray-icon-menu');
+    BrowserWindow = require('browser-window'),
+    TrayMenu = require('./js/tray-icon-menu');
 application.on('ready', function() {
-
     var mainWindow = new BrowserWindow({
         width: 600,
         height: 300,
         center:true,
         title:"Electron Tray Icon Menu Demo",
     });
-
     TrayMenu.renderTrayIconMenu();
     mainWindow.loadUrl('file://' + __dirname + '/main.html');
     mainWindow.on('closed', function() {
